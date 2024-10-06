ba0330f520c51c2d182926eeeb01c716a19eb5ac
c21xdx <c21xdx@gmail.com>
2024-10-06 21:41:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-sed -i "s/8080/$PORT/" /app/config.json
 sed -i "s/app_passid/$PASSID/" /app/config.json
-/app/xray -c /app/config.json
+/app/xray -c /app/config.json &
+caddy reverse-proxy --from :$CADDY_PORT --to :3333
